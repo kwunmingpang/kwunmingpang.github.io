6b39ed8e2bc8b0529e2277b8fd6e045d5d5790a3
kwunmingpang <pangkwunming@gmail.com>
2023-11-14 19:24:17 +1100

    docs

--- next.config.mjs
+++ next.config.mjs
@@ -17,7 +17,7 @@ const withMDX = createMDX(
 const nextConfig = {
     output: "export",
     pageExtensions: ['js', 'jsx', 'mdx', 'md', 'ts', 'tsx'],
-    distDir: "/docs"
+    // distDir: "/docs"
 }
 
 // module.exports = withMDX(nextConfig)
