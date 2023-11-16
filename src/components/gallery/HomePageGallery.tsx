02744fd8ade12d83b6970d27867499eec654acb7
kwunmingpang <pangkwunming@gmail.com>
2023-11-17 03:05:48 +1100

    fix gallery lag

--- src/components/gallery/HomePageGallery.tsx
+++ src/components/gallery/HomePageGallery.tsx
@@ -23,30 +23,40 @@ const DisplayImage: FC<{ pic: string; }> = ({ pic }) => {
     </div>
 }
 
+const DisplayImageMobile: FC<{ pic: string; }> = ({ pic }) => {
+    const router = useRouter()
+    const imgOnClick = useCallback(() => {
+        router.push("/gallery")
+    }, [router])
+
+    return <div className={`${style.emblaSlide} grow-0 shrink-0 cursor-pointer`} onClick={imgOnClick} >
+        <Image
+            className="h-64 w-64 object-cover rounded-lg ring-1 ring-zinc-400/5 rounded-lg"
+            src={pic} width={480} height={480} alt="pictures in gallery" />
+    </div>
+}
+
 const HomePageGallery: FC<{ pictures: string[] }> = ({ pictures }) => {
     const [emblaRef] = useEmblaCarousel(OPTIONS as any)
 
-    return <div ref={emblaRef}>
-        <div className={`${style.emblaContainer} flex gap-2`}>
+    return <>
+        <div className={`${style.emblaContainer} overflow-x-scroll flex gap-2 lg:hidden`}>
             {
                 pictures?.map((pic, index) => {
-                    return <DisplayImage pic={pic} key={`${pic}_${index}`} />
+                    return <DisplayImageMobile pic={pic} key={`${pic}_${index}`} />
                 })
             }
         </div>
-    </div>
-    // return <div className="flex gap-6 w-screen -ml-32 overflow-hidden">
-    //     {
-    //         pictures?.map((pic, index) => {
-    //             return <Link href={"/gallery"} key={`${pic}_${index}`}>
-    //                 <Image
-    //                     className="h-64 w-64 object-cover rounded-lg ring-1 ring-zinc-400/5 rounded-lg opacity-80 hover:opacity-100 transition duration-300"
-    //                     src={pic} width={480} height={480} alt="pictures in gallery"
-    //                     quality={25} priority={false} loading='lazy' />
-    //             </Link>
-    //         })
-    //     }
-    // </div>
+        <div ref={emblaRef}>
+            <div className={`${style.emblaContainer} gap-2 hidden lg:flex`}>
+                {
+                    pictures?.map((pic, index) => {
+                        return <DisplayImage pic={pic} key={`${pic}_${index}`} />
+                    })
+                }
+            </div>
+        </div>
+    </>
 }
 
 export default HomePageGallery
