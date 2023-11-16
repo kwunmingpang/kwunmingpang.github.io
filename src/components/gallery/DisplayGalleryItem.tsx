af13d72ae0185d3f933c44333c07b027afb9123d
kwunmingpang <pangkwunming@gmail.com>
2023-11-17 03:11:22 +1100

    fix lazy fetch gallery

--- src/components/gallery/DisplayGalleryItem.tsx
+++ src/components/gallery/DisplayGalleryItem.tsx
@@ -6,8 +6,10 @@ const DisplayGalleryItem: FC<{ pictureSource: string; handleOpen: any; setModalI
         setModalImage(pictureSource)
         handleOpen()
     }
-    return <div className="relative cursor-pointer" onClick={itemOnClick}>
-        <Image className="h-full w-auto object-cover rounded-lg ring-1 ring-zinc-400/5" src={pictureSource} width={720} height={480} alt="pictures in gallery" />
+    return <div className="relative cursor-pointer" style={{ minHeight: 150 }} onClick={itemOnClick}>
+        <Image className="h-full w-auto object-cover rounded-lg ring-1 ring-zinc-400/5"
+            src={pictureSource} width={720} height={480}
+            alt="pictures in gallery" />
         <div className="inset-0 absolute blur overflow-hidden bg-blue-300/25 bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
     </div>
 }
