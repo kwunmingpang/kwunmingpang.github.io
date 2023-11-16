5f2ff31720992cbfb63c9f958c9f8fe16b46d066
kwunmingpang <pangkwunming@gmail.com>
2023-11-17 02:53:13 +1100

    main layout

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -3,7 +3,7 @@ import { FC, PropsWithChildren } from "react"
 const MainLayout: FC<PropsWithChildren> = ({ children }) => {
     return <div className="flex w-full overflow-x-hidden">
         <div className="fixed inset-0 flex justify-center sm:px-8 lg:px-20 xl:px-44 -z-10">
-            <div className="w-full ring-1 bg-zinc-800/50 ring-zinc-200/10"></div>
+            <div className="w-full ring-1 bg-zinc-800/50 ring-zinc-200/10 pb-16"></div>
         </div>
         <div className="relative flex w-full flex-col sm:px-8 lg:px-20 xl:px-44">
             {children}
